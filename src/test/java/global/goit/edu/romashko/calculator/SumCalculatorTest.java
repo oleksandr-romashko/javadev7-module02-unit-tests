fd6f67bcb8afd790836e2d3c4b830ad2d0b3751f
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-10-13 00:19:03 +0200

    Add init calculator test class

---
+++ src/test/java/global/goit/edu/romashko/calculator/SumCalculatorTest.java
@@ -0,0 +1,12 @@
+package global.goit.edu.romashko.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class SumCalculatorTest {
+    private SumCalculator calculator;
+
+    @BeforeEach
+    void init() {
+        calculator = new SumCalculator();
+    }
+}
