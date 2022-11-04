efc42999e04a60d1efa1a9c204779964a93285e5
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-11-04 21:18:49 +0100

    Add unit test cases

--- src/test/java/global/goit/edu/romashko/calculator/SumCalculatorTest.java
+++ src/test/java/global/goit/edu/romashko/calculator/SumCalculatorTest.java
@@ -1,6 +1,11 @@
 package global.goit.edu.romashko.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SumCalculatorTest {
     private SumCalculator calculator;
@@ -8,5 +13,35 @@ public class SumCalculatorTest {
     @BeforeEach
     void init() {
         calculator = new SumCalculator();
+    }
+
+    @AfterEach
+    void teardown() {
+        calculator = null;
+    }
+
+    @Test
+    public void shouldReturnOneWhenInputIsOne() {
+        int actual = calculator.sum(1);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnSumWhenInputIsMoreThanOne() {
+        int actual = calculator.sum(3);
+        int expected = 6;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenInputIsZero() {
+        String expectedMessage = "bad value - value is less than 1";
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> calculator.sum(0));
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
