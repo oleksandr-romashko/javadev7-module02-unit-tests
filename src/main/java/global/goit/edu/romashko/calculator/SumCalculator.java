7b9f3e3617cc120202020f10d6d486593d0cf211
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-11-04 21:19:19 +0100

    Add task code implementation

--- src/main/java/global/goit/edu/romashko/calculator/SumCalculator.java
+++ src/main/java/global/goit/edu/romashko/calculator/SumCalculator.java
@@ -2,6 +2,13 @@ package global.goit.edu.romashko.calculator;
 
 public class SumCalculator {
     public int sum(int n) {
-        return -1;
+        if (n <= 0) {
+            throw new IllegalArgumentException("bad value - value is less than 1");
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
     }
 }
